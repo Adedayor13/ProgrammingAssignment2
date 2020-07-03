1e5aa9e4e8758751aecfcf8118e03c4a40a7140f
Adedayor13 <66908264+Adedayor13@users.noreply.github.com>
2020-07-03 12:44:16 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -29,3 +29,10 @@ cacheSolve <- function(x, ...) {
         x$setinverse(m)
         m
 }
+
+
+##Testing Example
+c <- matrix(c(4,3,2,1),2,2)
+c1 <- makeCacheMatrix(c) ##code to make Cache matrix of c
+c2 <- cacheSolve(c1) #code to return inverse from Cache
+c2 ##inverse retruned from Cache
